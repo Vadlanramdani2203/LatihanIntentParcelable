f4bb940173698b6d183fc52254ee255a24bf865c
Vadlanramdani2203 <vadlanramdani22.siswa@smkn4bdg.sch.id>
2020-08-23 18:55:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanintentparcelable/Activity_move.java
@@ -0,0 +1,11 @@
+package com.example.latihanintentparcelable;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class Activity_move extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
